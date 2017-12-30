218fa079982f947711900a13628bcbef314dbb3c
wuweinan <573877858@qq.com>
2017-12-30 07:03:05 -0800

    add requirements.txt

--- mblog/urls.py
+++ mblog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from mainsite.views import homepage
 
 urlpatterns = [
+	url(r'^$', homepage),
     url(r'^admin/', include(admin.site.urls)),
 ]
