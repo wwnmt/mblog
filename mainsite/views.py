b7ce7babfb1c48156235d46bd26fa8f7856b3407
wuweinan <573877858@qq.com>
2017-12-30 08:02:19 -0800

    post_detail

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
+
 
 # Create your views here.
 def homepage(request):
+	template = get_template('index.html')
 	posts = Post.objects.all()
-	post_lists = list()
-	for count, post in enumerate(posts, 1):
-		post_lists.append("No.{}:".format(str(count)) + str(post.title)+"<hr>")
-		post_lists.append("<small>" + str(post.body) + "</small><br><br>")
-	return HttpResponse(post_lists)
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
+
+
+def showpost(request, slug):
+	template = get_template('post.html')
+	try:
+		post = Post.objects.get(slug=slug)
+		if post != None:
+			html = template.render(locals())
+			return HttpResponse(html)
+	except:
+		return redirect('/')
